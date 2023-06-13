9eb46c8514a1ef2e93adc3a732585730a8acb15d
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-13 10:47:33 +0200

    set and formatted data

--- src/org/lessons/java/eventi/Evento.java
+++ src/org/lessons/java/eventi/Evento.java
@@ -1,4 +1,78 @@
 package org.lessons.java.eventi;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
+    //FIELDS
+    private String titolo;
+    private LocalDate data;
+    private int postiASedereTotali;
+    private int numeroPrenotati;
+
+
+    //CONSTRUCTOR
+
+    public Evento(String titolo, LocalDate data, int postiASedereTotali) {
+        this.titolo = titolo;
+        setData(data);
+        this.postiASedereTotali = postiASedereTotali;
+        this.numeroPrenotati = 0;
+
+    }
+
+    //GETTERS
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public LocalDate getData() {
+
+        return data;
+    }
+
+    public int getPostiASedereTotali() {
+        return postiASedereTotali;
+    }
+
+    public int getNumeroPrenotati() {
+        return numeroPrenotati;
+    }
+
+
+    // SETTERS
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setData(LocalDate data) {
+        if (data.isBefore(LocalDate.now())){
+            throw new RuntimeException ("Hai cercato di prenotare un Evento gia' concluso");
+        }
+
+        this.data = data;
+    }
+
+    //METHODS
+    public int prenota(){
+
+    }
+
+    public int disdici(){
+
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Evento del :{" + data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy,")) +
+                "dal titolo='" + titolo + '\'' +
+                '}';
+    }
 }
