ebfaf74d3eeb98eaf615a38e2e7772d4af2f6d6e
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-13 11:22:00 +0200

    Milestone 1 con errori (devo creare ancora la var postiPrenotati)

--- src/org/lessons/java/eventi/Evento.java
+++ src/org/lessons/java/eventi/Evento.java
@@ -1,8 +1,9 @@
 package org.lessons.java.eventi;
 
-import java.math.BigDecimal;
+
+import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.time.format.DateTimeFormatter;
 
 public class Evento {
@@ -48,24 +49,52 @@ public class Evento {
     public void setTitolo(String titolo) {
         this.titolo = titolo;
     }
-
+    //    La data dell'evento non deve essere passata'
     public void setData(LocalDate data) {
         if (data.isBefore(LocalDate.now())){
-            throw new RuntimeException ("Hai cercato di prenotare un Evento gia' concluso");
+            throw new DateTimeException ("La data inserita e' gia' passata");
         }
-
         this.data = data;
+    }
+//    I posti a sedere devono essere (esempio forum assago) piu' di 0 e meno di 12700'
+    public void setPostiASedereTotali(){
+    if (postiASedereTotali <= 0){
+        throw new RuntimeException("Il numero di posti deve essere positivo!");
+    }
+    if (postiASedereTotali > 12700){
+        throw new RuntimeException("Il numero massimo di posti del forum e' 12700");
+    }
     }
 
     //METHODS
+
+//    La prenotazione deve agg. uno o piu'(questo se riesco) alle prenotazioni e lanciare eccezioni se evento passato o finiti i posti,tentato if (!setData(LocalDate data) ma non funziona
     public int prenota(){
+        if (data.isBefore(LocalDate.now())){
+            throw new DateTimeException("Hai cercato di prenotare un Evento gia' concluso");
+        }
+        if(postiPrenotati > postiDisponibili()){
+            throw new RuntimeException("Non ci sono abbastanza biglietti disponibili per soddisfare la tua richiesta")
+        }
 
+    return numeroPrenotati++;
     }
-
+//
     public int disdici(){
-
+    if (data.isBefore(LocalDate.now())){
+        throw new DateTimeException("Hai cercato di prenotare un Evento gia' concluso");
+    }
+    if (numeroPrenotati <= 0){
+        throw new RuntimeException("Nessuna Prenotazione registrata")
+    }
+    return numeroPrenotati--;
     }
 
+
+
+//    public int postiDisponibili(){
+//        return postiASedereTotali - postiPrenotati;
+//    }
 
 
 
